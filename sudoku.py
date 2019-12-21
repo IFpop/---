ff33349fc30f5d92a5dec9e4c1df96cbc8988cca
IFpop <965529474@qq.com>
2019-12-21 12:25:01 +0800

    增添-s功能以及处理文本格式

--- sudoku.py
+++ sudoku.py
@@ -1,23 +1,61 @@
-#-*-coding = utf-8 -*-
+# -*-coding = utf-8 -*-
 #owner: IFpop
 #time: 2019/12/20
 
 import sudoku_generate
+import sudo_solve
+from copy import deepcopy
 import sys
 import time
 
 try:
-    # command=raw_input("please input command\n")
-    type = sys.argv[1]
-    print(type)
-    if type[1] == 'c':
+    cmd = sys.argv[1]
+    print("cmd:"+cmd)
+    # 如果是-c的话。就执行create
+    if cmd[1] == 'c':
+        # 确定开始时间
         start = time.process_time()
         num = int(sys.argv[2])
+        # 开始生成终局
         sudoku_generate.create_sudoku(num)
+        # 确定结束时间
         end = time.process_time()
         print("running time is %6.4f" % (end-start))
+    elif cmd[1] == 's':
+        start = time.process_time()
+        # 第二个参数应该是数独文件路径
+        path = sys.argv[2]
+        # 打开文件
+        with open(path, 'r') as f:
+            line = f.readline()
+            # 做个标记位，测试用
+            num = 0
+            # 定义一个list记录当前数组
+            sudoku = []
+            while line:
+                if(line[0] != '\n' and num != 9):
+                    # 将每一行转成数字
+                    line = [int(i) for i in line if str.isdigit(i)]
+                    # print(line)
+                    sudoku.append(deepcopy(line))
+                    # print(num)
+                    num += 1
+                    line = f.readline()
+                # 已经收集了一个数独
+                elif num == 9 or line[0] == '\n':
+                    line = f.readline()
+                    num = 0
+                    sudo = sudo_solve.Solve_sudoku(sudoku)
+                    print(u"完成，猜测了%s次" % sudo.guess_times)
+                    print(sudo.value)
+                    sudoku = []
+            # 最后一个
+            sudo = sudo_solve.Solve_sudoku(sudoku)
+            print(u"完成，猜测了%s次" % sudo.guess_times)
+            print(sudo.value)
+
 
 except ValueError:
-        print("please input correct number")
+    print("please input correct number")
 except IOError:
-    print("Error: 没有找到文件或读取文件失败")
+    print("Error: Not find or open failed!")
