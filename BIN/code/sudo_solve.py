7fa5fa318a77f0de3d25233e01825e201910b6b6
IFpop <965529474@qq.com>
2020-01-18 19:04:10 +0800

    将数独包装成exe文件

---
+++ BIN/code/sudo_solve.py
@@ -0,0 +1,425 @@
+#coding=utf-8
+#owner: IFpop
+#time: 2019/12/25
+'''
+排除候选猜测法
+- 唯一解法
+- 基础摒弃法
+- 隐性唯一候选法
+- 候选数区块减法
+'''
+
+import numpy as np
+from queue import Queue, LifoQueue
+from copy import deepcopy
+import time
+import array
+
+'''
+存储需要猜测点的信息
+'''
+class Recorder:
+    point = None  # 进行猜测的点
+    pindex = 0   # 猜测候选列表使用的值的索引
+    cur_value = None # 回溯记录的值
+
+class SudoKu:
+    def __init__(self,data:list)->None:
+        self.sudoku = np.array(data) #数据初始化
+        self.value =np.array([[0] * 9] * 9, dtype=object) #当前数独初始化
+        self.runtime = 0 #初始化运行时间
+        self.know_points = Queue() #先进先出，新解(已解决值)的点对
+        self.recorder = LifoQueue() #先进后出，回溯器
+        self.guess_times = 0
+
+         # 对数独进行处理
+        for row in range(9):
+            for col in range(9):
+                if self.sudoku[row][col]:  # 如果是已确定点
+                    self.value[row][col] = int(self.sudoku[row][col])
+                    # 新的确认的值添加到列表中，以便遍历
+                    self.know_points.put((row, col))
+                else:
+                    self.value[row][col] = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+class Solve_sudo:
+    '''
+    Solve sudo class:
+    - init          #初始化
+    - sudoku_solve  #解决数独
+    - sudo_exclude  #排除候选值
+    '''
+    def __init__(self,filepath:str)->None:
+        #先将所有的答案记录下来，最后一起存储
+        ans = []
+        # 建立buf缓存区
+        buf = ""
+
+        # 九宫格的基准元组，将列表变为元组会使速度上升
+        self.base_points = ((0,0),(0,3),(0,6),(3,0),(3,3),(3,6),(6,0),(6,3),(6,6))
+        # 打开文件
+        with open(filepath, 'r') as f:
+            line = f.readline()
+            num = 0  #记录当前是否是第九行
+            cur = 0  #记录当前个数
+            temp = []
+            while line:
+                if(line[0] != '\n' and num != 9):
+                    line = [int(i) for i in line if str.isdigit(i)]
+                    temp.append(deepcopy(line))
+                    num += 1
+                    line = f.readline()
+                # 已经收集了一个数独
+                elif num == 9 or line[0] == '\n':
+                    line = f.readline()
+                    num = 0
+                    # print(cur)
+                    cur += 1
+                    self.current_sudoku = SudoKu(temp)
+                    # start = time.time()
+                    self.sudo_solve()
+                    self.current_sudoku.value = self.current_sudoku.value.tolist()
+                    ans.append(self.current_sudoku.value)
+                    # end = time.time()
+                    # print("time is %.4f" % (end-start))
+                    temp = []
+        # print(cur)
+        # 最后一个
+        self.current_sudoku = SudoKu(temp)  
+        start = time.time()
+        self.sudo_solve()
+        self.current_sudoku.value = self.current_sudoku.value.tolist()
+        ans.append(self.current_sudoku.value)
+        end = time.time()
+        # print("time is %.4f" % (end-start))
+
+        # print(ans)
+        # with open('sudoku.txt','w') as f:
+        #     for item in ans:
+        #         for index in range(9):
+        #             f.write(("%s" % item[index])[1:-1].replace(', ',' '))
+        #             f.write('\n')
+        #         f.write('\n')
+        # f.close()
+        temp = 0
+        for item in ans:
+            for index in range(9):
+                buf += str(item[index])[1:-1].replace(', ',' ')
+                if(temp == cur and index == 8) != 1:
+                    buf += "\n"
+            if(temp != cur):
+                buf += "\n"
+            temp += 1
+        # print(buf)
+        with open("sudoku.txt",'w+') as f:
+            f.write(buf)
+        f.close()
+        #print("done")
+        
+    #采用合适的算法，排除候选
+    def sudo_exclude(self):
+        type_same = True
+        type_one = True
+
+        while type_same:
+            while type_one:               
+                self.remove_konw_num()
+                # 检查List里值为单个数字的情况，如有新answer则加入current_sudoku.know_points Queue，立即cut_num
+                type_one = self.Only_one_exisitence()
+            # print(self.current_sudoku.value)
+            # 检查同行或列的情况
+            type_same = self.Hidden_exclusion()
+            type_one = True
+        # 设置返回值，用于单元测试
+        return self.current_sudoku.value
+
+    '''
+    Remove the numbers in the candidate codes in the row, column and nine palace lattice according to each determined point
+    '''
+    def remove_konw_num(self):
+        # 剔除数字
+        while not self.current_sudoku.know_points.empty():
+            point = self.current_sudoku.know_points.get()  # 先进先出
+
+            # 获取该点的横纵列坐标
+            row, col = point
+            # 通过坐标获取对应值
+            val = self.current_sudoku.value[row, col]
+
+            # 检查行
+            for i, item in enumerate(self.current_sudoku.value[row]):
+                if(isinstance(item, list)):
+                    # 统计该点在该行出现的次数
+                    if(item.count(val)):
+                        item.remove(val)  # 移除
+                        # 判断移除后，是否剩下一个元素
+                        if(len(item) == 1):
+                            self.current_sudoku.know_points.put((row, i))  # 添加该坐标到已解决
+                            self.current_sudoku.value[row][i] = item[0]
+            # 检查列
+            for i, item in enumerate(self.current_sudoku.value[:, col]):
+                if isinstance(item, list):
+                    if item.count(val):
+                        item.remove(val)
+
+                        # 判断移除后，是否剩下一个元素
+                        if len(item) == 1:
+                            self.current_sudoku.know_points.put((i, col))
+                            self.current_sudoku.value[i][col] = item[0]
+            # 检查九宫格
+            # 先找到九宫格基准点，也就是3×3数组的左上角的点
+            bp_row, bp_col = map(lambda x: x // 3 * 3, point)
+            # print(bp_row, bp_col)
+            # 在3×3的矩阵里进行排除
+            for m_r, row in enumerate(self.current_sudoku.value[bp_row:bp_row + 3, bp_col:bp_col + 3]):
+                for m_c, item in enumerate(row):
+                    if(isinstance(item, list)):
+                        if(item.count(val)):
+                            item.remove(val)
+
+                            # 判断移除后
+                            if len(item) == 1:
+                                r = bp_row + m_r
+                                c = bp_col + m_c
+                                self.current_sudoku.know_points.put((r, c))
+                                self.current_sudoku.value[r][c] = item[0]
+
+        # 用于单元测试
+        return self.current_sudoku.value
+
+    '''
+    For a class that can directly determine other rows, columns or nine palace cells, the point can be directly determined
+    '''
+    def Only_one_exisitence(self)->None:
+        # 同一行只有一个数字的情况
+        for row in range(9):
+            # 只取出的是这一行是list格子
+            vals = list(filter(lambda x: isinstance(x, list), self.current_sudoku.value[row]))
+            # print(val) #得到该行的所有可能性的二维数组
+            for col, item in enumerate(self.current_sudoku.value[row]):
+                if(isinstance(item, list)):
+                    for value in item:  # 对其中的每个元素判断，如果只出现一次则确定
+                        if(sum(map(lambda x: x.count(value), vals)) == 1):
+                            self.current_sudoku.value[row][col] = value
+                            self.current_sudoku.know_points.put((row, col))
+                            return True
+        # 对于列
+        for col in range(0, 9):
+            vals = list(
+                filter(lambda x: isinstance(x, list), self.current_sudoku.value[:, col]))
+
+            for row, item in enumerate(self.current_sudoku.value[:, col]):
+                if(isinstance(item, list)):
+                    for value in item:
+                        if(sum(map(lambda x: x.count(value), vals)) == 1):
+                            self.current_sudoku.value[row][col] = value
+                            self.current_sudoku.know_points.put((row, col))
+                            return True
+
+        # 对于九宫格
+        for row, col in self.base_points:
+            # reshape: 3x3 改为1维数组
+            vals = list(filter(lambda x: isinstance(x, list),
+                               self.current_sudoku.value[row:row + 3, col:col + 3].reshape(1, -1)[0]))
+            for m_r, rows in enumerate(self.current_sudoku.value[row:row + 3, col:col + 3]):
+                for m_c, item in enumerate(rows):
+                    if(isinstance(item, list)):
+                        for value in item:
+                            if(sum(map(lambda x: x.count(value), vals)) == 1):
+                                self.current_sudoku.value[row + m_r, col + m_c] = value
+                                self.current_sudoku.know_points.put((row + m_r, col + m_c))
+                                return True
+
+    '''
+    Invisible elimination of the same row in the nine palace grid
+    '''
+    def Hidden_exclusion(self)->None:
+        for bp_r, bp_c in self.base_points:
+            block = self.current_sudoku.value[bp_r:bp_r + 3, bp_c:bp_c + 3]
+
+            # 判断数字1~9在该九宫格的分布情况,reshape变为一维
+            _data = block.reshape(1, -1)[0]
+            for i in range(1, 10):
+                result = map(lambda x: 0 if not isinstance(x[1], list) else x[0] + 1 if x[1].count(i) else 0,
+                             enumerate(_data))
+                result = list(filter(lambda x: x > 0, result))
+                r_count = len(result)
+
+                if r_count in [2, 3]:
+                    # 2或3个元素才有可能同一行或同一列
+                    rows = list(map(lambda x: (x - 1) // 3, result))
+                    cols = list(map(lambda x: (x - 1) % 3, result))
+
+                    if len(set(rows)) == 1:
+                        # 同一行，去掉其他行的数字
+                        result = list(
+                            map(lambda x: bp_c + (x - 1) % 3, result))
+                        row = bp_r + rows[0]
+
+                        for col in range(0, 9):
+                            if not col in result:
+                                item = self.current_sudoku.value[row, col]
+                                if isinstance(item, list):
+                                    if item.count(i):
+                                        item.remove(i)
+
+                                        # 判断移除后，是否剩下一个元素
+                                        if len(item) == 1:
+                                            self.current_sudoku.know_points.put((row, col))
+                                            self.current_sudoku.value[row, col] = item[0]
+                                            return True
+                    elif len(set(cols)) == 1:
+                        # 同一列
+                        result = list(
+                            map(lambda x: bp_r + (x - 1) // 3, result))
+                        col = bp_c + cols[0]
+
+                        for row in range(0, 9):
+                            if not row in result:
+                                item = self.current_sudoku.value[row, col]
+                                if isinstance(item, list):
+                                    if item.count(i):
+                                        item.remove(i)
+
+                                        # 判断移除后，是否剩下一个元素
+                                        if len(item) == 1:
+                                            self.current_sudoku.know_points.put((row, col))
+                                            self.current_sudoku.value[row, col] = item[0]
+                                            return True
+                    
+        
+    #评分，找到最佳的猜测坐标
+    def get_best_point(self)->None:
+        best_score = 0
+        best_point = (0,0)
+
+        for row in range(9):
+            for col in range(9):
+                point_score = self.get_point_score((row,col))
+                if(best_score < point_score):
+                    best_score = point_score
+                    best_point = (row,col)
+        return best_point
+
+    def get_point_score(self,point:tuple)->None:
+        # 评分标准 (10-候选个数) + 同行确定数字个数 + 同列确定数字个数
+        row, col = point
+        item = self.current_sudoku.value[row][col]
+
+        if isinstance(item, list):
+            score = 10 - len(item)
+            for x in self.current_sudoku.value[row]:
+                if(isinstance(x,int)):
+                    score += 1
+            for x in self.current_sudoku.value[:,col]:
+                if(isinstance(x,int)):
+                    score += 1
+            return score
+        else:
+            return 0
+    
+    #验证有没错误
+    def check_value(self)->bool:
+        #行
+        r = 0
+        for row in self.current_sudoku.value:
+            nums = []
+            lists = []
+            for item in row:
+                if(isinstance(item,list)):
+                    lists.append(item)
+                else:
+                    nums.append(item)
+            if(len(set(nums)) != len(nums)):
+                return False
+            if len(list(filter(lambda x: len(x) == 0, lists))):
+                return False 
+            r += 1
+        
+        #列
+        for c in range(9):
+            nums = []
+            lists = []
+            col = self.current_sudoku.value[:,c]
+            for item in col:
+                if(isinstance(item,list)):
+                    lists.append(item)
+                else:
+                    nums.append(item)
+            if len(set(nums)) != len(nums):
+                return False
+            if len(list(filter(lambda x: len(x) == 0, lists))):
+                return False
+        
+        #九宫格
+        for b_r, b_c in self.base_points:
+            nums = []
+            lists = []
+            block = self.current_sudoku.value[b_r:b_r + 3, b_c:b_c + 3].reshape(1, -1)[0]
+
+            for item in block:
+                if(isinstance(item,list)):
+                    lists.append(item)
+                else:
+                    nums.append(item)
+            if len(set(nums)) != len(nums):
+                return False
+            if len(list(filter(lambda x: len(x) == 0, lists))):
+                return False 
+        return True
+    
+    # 猜测记录
+    def record_guess(self, point:tuple, index:int=0)->None:
+        # 记录
+        recorder = Recorder()
+        recorder.point = point
+        recorder.point_index = index
+        # recorder.value = self.current_sudoku.value.copy() #numpy的copy不行
+        recorder.value = deepcopy(self.current_sudoku.value)
+        self.current_sudoku.recorder.put(recorder)
+        self.current_sudoku.guess_times += 1  # 记录猜测次数
+
+        # 新一轮的排除处理
+        item = self.current_sudoku.value[point]
+        # assume only 1 in this point
+        self.current_sudoku.value[point] = item[index]
+        self.current_sudoku.know_points.put(point)
+        self.sudo_exclude()
+    
+    def reback(self)->None:
+        while True:
+            if(self.current_sudoku.recorder.empty()):
+                raise Exception('Sudoku is wroing, no answer!')
+            else:
+                recorder = self.current_sudoku.recorder.get()
+                point = recorder.point
+                index = recorder.point_index+1
+                item = recorder.value[point]
+
+                #判断索引是否超出范围
+                # if no exceed,则再回溯一次
+                if(index < len(item)):
+                    break
+                #if exceed,pop next recorder
+        self.current_sudoku.value = recorder.value
+        self.record_guess(point,index)
+
+    #main function解题
+    def sudo_solve(self)->None:
+        #第一次解题，排除法
+        self.sudo_exclude()
+        #检查有没有错误，有错误则回溯，没错误却未解开题目，则再猜测
+        while True:
+            if(self.check_value()):
+                fixed_answer = 0
+                for i in self.current_sudoku.value.reshape(1, -1)[0]:
+                    if(isinstance(i,int)):
+                        fixed_answer += 1
+                if(fixed_answer == 81):
+                    break
+                else:
+                    #获取最佳猜测点
+                    point = self.get_best_point()
+                    self.record_guess(point)
+            else:
+                self.reback()
---
+++ BIN/code/sudoku_generate.pyx
@@ -0,0 +1,152 @@
+#coding=utf-8
+#owner: IFpop
+#time: 2019/12/24
+
+'''
+这是cython优化之后的代码
+'''
+import sys
+import time
+from libc.stdio cimport FILE, fopen, fclose, fputs, fgets, feof, fputc
+
+move_way = (
+    (0, 3, 6, 1, 4, 7, 2, 5, 8),
+    (0, 3, 6, 1, 7, 4, 2, 5, 8),
+    (0, 3, 6, 4, 1, 7, 2, 5, 8),
+    (0, 3, 6, 4, 7, 1, 2, 5, 8),
+    (0, 3, 6, 7, 1, 4, 2, 5, 8),
+    (0, 3, 6, 1, 4, 7, 2, 8, 5),
+    (0, 3, 6, 1, 4, 7, 5, 2, 8),
+    (0, 3, 6, 1, 4, 7, 5, 8, 2),
+    (0, 3, 6, 1, 4, 7, 8, 2, 5),
+    (0, 3, 6, 1, 4, 7, 8, 5, 2),
+    (0, 3, 6, 1, 7, 4, 2, 8, 5),
+    (0, 3, 6, 4, 1, 7, 5, 2, 8),
+    (0, 3, 6, 4, 7, 1, 5, 8, 2),
+    (0, 3, 6, 7, 4, 1, 8, 2, 5),
+    (0, 3, 6, 7, 1, 4, 8, 5, 2),
+    (0, 6, 3, 1, 4, 7, 2, 5, 8),
+    (0, 6, 3, 1, 7, 4, 2, 5, 8),
+    (0, 6, 3, 4, 1, 7, 2, 5, 8),
+    (0, 6, 3, 4, 7, 1, 2, 5, 8),
+    (0, 6, 3, 7, 1, 4, 2, 5, 8),
+    (0, 6, 3, 1, 4, 7, 2, 8, 5),
+    (0, 6, 3, 1, 4, 7, 5, 2, 8),
+    (0, 6, 3, 1, 4, 7, 5, 8, 2),
+    (0, 6, 3, 1, 4, 7, 8, 2, 5),
+    (0, 6, 3, 1, 4, 7, 8, 5, 2),
+    (0, 6, 3, 1, 7, 4, 2, 8, 5),
+    (0, 6, 3, 4, 1, 7, 5, 2, 8),
+    (0, 6, 3, 4, 7, 1, 5, 8, 2),
+    (0, 6, 3, 7, 4, 1, 8, 2, 5),
+    (0, 6, 3, 7, 1, 4, 8, 5, 2),
+)
+
+cdef class create_sudoku:
+    cdef FILE* Save_txt  #创建文件类型
+    cdef list sudo_num,cur_first_row
+    cdef list perm          #记录所有全排列 以5开头
+    cdef int cur            #用于生成全排列
+    cdef int index          #进行缓存区计数
+    cdef char buf[1000200]  #创建缓存区
+    cdef int sum            #记录num即需要生成的数独总数
+
+    def __init__(self,num:int):
+        #打开文件句柄
+        self.Save_txt = fopen('sudoku.txt', 'w+')
+        if self.Save_txt == NULL:
+            print("open failed!")
+            sys.exit(0)
+        
+        # 生成初始的第一行1-9
+        self.sudo_num = list(range(1, 10))
+        # 将学号移动到前列
+        self.sudo_num.remove(5)
+        #记录当前第一行
+        self.cur_first_row = [5]+self.sudo_num
+        # 用于生成全排列
+        self.cur = 1
+        # 初始化全排列
+        self.perm = []
+
+        # 初始化缓存区
+        self.index = 0
+
+        #计时加运行
+        cdef double start_time = time.time()
+        self.sum = num
+        self.create()
+        cdef double end_time = time.time()
+        #print("running time: %.4f" % (end_time-start_time))
+
+    '''
+    将数独以字符串的形式写入缓存区
+    '''
+    cdef void write2buf(self,sudoku:list,n:int):
+        cdef int i
+        cdef int j
+        if n != 0:
+            self.buf[self.index] = 10 # '\n'
+            self.index += 1
+        for i in range(9):
+            for j in range(9):
+                if j != 0:
+                    self.buf[self.index] = 32  # ' '
+                    self.index += 1
+                self.buf[self.index] = sudoku[i][j] + 48 # 0
+                self.index += 1
+            if(n == self.sum-1 and i == 8):
+                return
+            self.buf[self.index] = 10
+            self.index += 1
+        #print(len(self.buf)) 
+    
+    '''
+    根据第一行生成数独
+    '''
+    cdef get_sudoku(self,n:int,rows:list):
+        # 临时变量存取数独
+        cdef list Grid_sudoku = []
+        num = n % 30
+        for i in move_way[num]:
+            Grid_sudoku.append(rows[-i:]+rows[:-i])
+        return Grid_sudoku
+    
+    '''
+    递归计算出所有全排列结果
+    '''
+    cdef permutation(self,list a_row):
+        cdef list temp
+        if not a_row:
+            self.perm.append(list(self.cur_first_row))
+            return
+        for i in a_row:
+            self.cur_first_row[self.cur] = i
+            self.cur += 1
+            temp = list(a_row)
+            temp.remove(i)
+            self.permutation(temp)
+            self.cur -= 1
+    '''
+    主控函数
+    '''
+    cdef create(self):
+        cdef int i
+        cdef int tag  #当前全排列
+        # 生成所有全排列
+        self.permutation(self.sudo_num)
+        # 0~num-1
+        for i in range(self.sum-1):
+            tag = i % 40320 
+            # 将数独生成后，写入缓存区
+            self.write2buf(self.get_sudoku(i,self.perm[tag]),i)
+            # 缓存区已满，输入，然后初始化
+            if self.index > 1000000:
+                self.buf[self.index] = 0
+                fputs(self.buf,self.Save_txt)
+                self.index = 0
+        
+        tag = (self.sum-1) % 40320
+        self.write2buf(self.get_sudoku(i,self.perm[tag]), self.sum-1)
+        fputs(self.buf, self.Save_txt)
+        fclose(self.Save_txt)
